480336cb5212569e4e8f88dffcfa2b9a9677f960
buson94 <marvin.marken@gmail.com>
2025-03-05 23:35:21 +0100

    feat(scripts/setup.ps1): add step counter into echo strings

--- home/scripts/setup.ps1
+++ home/scripts/setup.ps1
@@ -1,17 +1,17 @@
 $step = 1
 $steps = 5
 
-echo "(1/$steps) Install important terminal tools"
+echo "($($step++)/$($steps)) Install important terminal tools"
 winget install -e --id Git.Git Microsoft.PowerShell JanDeDobbeleer.OhMyPosh
 
-echo "(2/$steps) Install other important tools"
+echo "($($step++)/$($steps)) Install other important tools"
 winget install -e --id 7zip.7zip Microsoft.PowerToys Mozilla.Thunderbird.de Google.Chrome.EXE
 
-echo "(3/$steps) Install communication tools"
+echo "($($step++)/$($steps)) Install communication tools"
 winget install -e --id Telegram.TelegramDesktop Discord.Discord OpenWhisperSystems.Signal
 
-echo "(4/$steps) Install basic dev tools"
+echo "($($step++)/$($steps)) Install basic dev tools"
 winget install -e --id JesseDuffield.lazygit Microsoft.VisualStudioCode JetBrains.WebStorm WinSCP.WinSCP PuTTY.PuTTY
 
-echo "(5/$steps) Install programming languages"
+echo "($($step++)/$($steps)) Install programming languages"
 winget install -e --id Oracle.JavaRuntimeEnvironment OpenJS.NodeJS.LTS

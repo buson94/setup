2f11b291a2578f8170d7f767a13b80256190b017
Buson94 <marvin.marken@gmail.com>
2025-03-06 11:06:59 +0100

    feat(scripts/setup.ps1): add package Nano and change some echo messages

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -4,18 +4,18 @@ $steps = 5
 $setupType = Read-Host "What setup to run? (all/basic)"
 
 echo "($($step++)/$($steps)) Install important terminal tools"
-winget install -e --id Git.Git Microsoft.PowerShell JanDeDobbeleer.OhMyPosh
+winget install -e --id Git.Git Microsoft.PowerShell JanDeDobbeleer.OhMyPosh GNU.Nano
 
 echo "($($step++)/$($steps)) Install other important tools"
 winget install -e --id 7zip.7zip Microsoft.PowerToys Mozilla.Thunderbird.de Google.Chrome.EXE
 
-echo "($($step++)/$($steps)) Install communication tools"
+echo "($($step++)/$($steps)) Install messenger"
 winget install -e --id Telegram.TelegramDesktop Discord.Discord OpenWhisperSystems.Signal
 
 echo "($($step++)/$($steps)) Install basic dev tools"
 winget install -e --id JesseDuffield.lazygit Microsoft.VisualStudioCode JetBrains.WebStorm WinSCP.WinSCP PuTTY.PuTTY
 
-echo "($($step++)/$($steps)) Install programming languages"
+echo "($($step++)/$($steps)) Install programming languages, frameworks etc."
 winget install -e --id Oracle.JavaRuntimeEnvironment OpenJS.NodeJS.LTS
 
 if ($setupType -contains 'all') {
